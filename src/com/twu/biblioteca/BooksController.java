d015e5c0a5ec94e45ec830d8fbf6b92aa1b44408
Simon Weiler <sweiler@thoughtworks.com>
2017-01-13 15:59:30 +0100

    @sweiler, @dazayzeh, @mShahidAslam: Succesful Checkout, UnSuccesful Checkout, Return Book, Succesful Return, Unsuccessful Return.

--- src/com/twu/biblioteca/BooksController.java
+++ src/com/twu/biblioteca/BooksController.java
@@ -10,18 +10,18 @@ public class BooksController {
 
     PrintStream printStream;
 
-    private final Map<Integer,Book> books = new HashMap<>();
+    private final Map<Integer, Book> books = new HashMap<>();
 
     public BooksController(PrintStream printStream) {
         this.printStream = printStream;
-        books.put(1,new Book("ABC", "Simon", 2017,1,false));
-        books.put(2,new Book("DEF", "Nana", 2017,2,false));
+        books.put(1, new Book("ABC", "Simon", 2017, 1, false));
+        books.put(2, new Book("DEF", "Nana", 2017, 2, false));
     }
 
     public List<Book> listAvailableBooks() {
         List<Book> books = new ArrayList<>();
-        for (Book book: this.books.values()) {
-            if(!book.isCheckedOut()) {
+        for (Book book : this.books.values()) {
+            if (!book.isCheckedOut()) {
                 books.add(book);
             }
         }
@@ -30,7 +30,7 @@ public class BooksController {
 
     public void getAllBooksWithDetails() {
         StringBuffer buffer = new StringBuffer();
-        for (Book book: books.values()) {
+        for (Book book : books.values()) {
             buffer.append(book.getTitle());
             buffer.append(" | ");
             buffer.append(book.getAuthor());
@@ -42,7 +42,23 @@ public class BooksController {
     }
 
     public void checkoutBook(int bookID) {
-         books.get(bookID).setCheckedOut(true);
+        Book book = books.get(bookID);
 
+        if (book.isCheckedOut()) {
+            printStream.println("That book is not available.");
+        } else {
+            book.setCheckedOut(true);
+            printStream.println("Thank you! Enjoy the book.");
+        }
+    }
+
+    public void returnBook(int bookID) {
+        Book book = books.get(bookID);
+        if (book.isCheckedOut()) {
+            book.setCheckedOut(false);
+            printStream.println("Thank You for returning the book.");
+        } else {
+            printStream.println("That is not a valid book to return.");
+        }
     }
 }
--- test/com/twu/biblioteca/BooksControllerTest.java
+++ test/com/twu/biblioteca/BooksControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.util.Arrays;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -30,5 +31,40 @@ public class BooksControllerTest {
     public void shouldCheckoutABook() {
         booksController.checkoutBook(1);
         assertThat(booksController.listAvailableBooks(),is(Arrays.asList(new Book("DEF","Nana",2017,2,false))));
+    }
+
+    @Test
+    public void shouldPrintSuccessfulCheckoutMessageForCheckoutABook() {
+        booksController.checkoutBook(1);
+        assertThat(advancedPrintStream.getContents(), containsString("Thank you! Enjoy the book.\n"));
+    }
+
+    @Test
+    public void shouldPrintUnsuccessfulCheckoutMessageForCheckoutABook() {
+        booksController.checkoutBook(1);
+        booksController.checkoutBook(1);
+        assertThat(advancedPrintStream.getContents(), containsString("That book is not available.\n"));
+    }
+
+    @Test
+    public void shouldReturnABook() {
+        booksController.checkoutBook(1);
+        booksController.returnBook(1);
+        assertThat(booksController.listAvailableBooks(),is(Arrays.asList(new Book("ABC", "Simon", 2017,1,false),
+                new Book("DEF","Nana",2017,2,false))));
+    }
+
+    @Test
+    public void shouldPrintSuccessfulMessageOnReturningABook() {
+        booksController.checkoutBook(1);
+        booksController.returnBook(1);
+        assertThat(advancedPrintStream.getContents(), containsString("Thank You for returning the book.\n"));
+    }
+
+    @Test
+    public void shouldPrintUnSuccessfulMessageOnReturningABook() {
+        booksController.checkoutBook(1);
+        booksController.returnBook(2);
+        assertThat(advancedPrintStream.getContents(), containsString("That is not a valid book to return.\n"));
     }
 }
